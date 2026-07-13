c30e7e44b8431aa389c7bf90df75e077f0cee5ff
jithender-dev <jithender2003b@gmail.com>
2026-07-13 11:03:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,11 @@ const initializeDBAndServer = async () => {
 
     await db.exec(schema)
 
-    app.listen(5000, () => {
-      console.log("Server Running at http://localhost:5000/")
-    })
+    const PORT = process.env.PORT || 5000;
+
+   app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
 
   } catch (e) {
     console.log(`DB Error: ${e.message}`)
